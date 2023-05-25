2216c4e035a5b177c87a2d916d008471d5b5520f
mcourtade024380 <mcourtade024380@219B-5-23>
2023-05-24 20:44:59 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Edibelle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // MY Connection String
+        /*
+         * Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\Edibelle\DB\edibelle.mdf;Integrated Security = True; Connect Timeout = 30
+        */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
