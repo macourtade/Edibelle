2f980d3b4b0de43c6b705487c31d115a278d9d83
Morgan Courtade <mcourtade024380@students.iticollege.edu>
2023-06-07 21:17:07 -0500

    added forms

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Edibelle
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DepartmentForm());
         }
     }
 }
