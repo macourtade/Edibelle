46edb3c9ea38b4de30f0ae61c9a66a1c6c4dfbe1
Morgan Courtade <mcourtade024380@students.iticollege.edu>
2023-06-29 19:37:56 -0500

    test 2

--- DepartmentForm.Designer.cs
+++ DepartmentForm.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnTest
             // 
-            this.button1.Location = new System.Drawing.Point(342, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnTest.Location = new System.Drawing.Point(342, 85);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "test";
+            this.btnTest.UseVisualStyleBackColor = true;
             // 
             // DepartmentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTest);
             this.Name = "DepartmentForm";
             this.Text = "Departments";
             this.Load += new System.EventHandler(this.DepartmentForm_Load);
@@ -55,6 +55,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTest;
     }
 }
