ef483cc568cd15cb327606de3f7b2bf75d839437
Morgan Courtade <mcourtade024380@students.iticollege.edu>
2023-06-29 19:37:02 -0500

    Test

--- DepartmentForm.Designer.cs
+++ DepartmentForm.Designer.cs
@@ -28,22 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // DepartmentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "DepartmentForm";
-
             this.Text = "Departments";
             this.Load += new System.EventHandler(this.DepartmentForm_Load);
-
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
